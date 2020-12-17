2f5eeeb67cbe387d920ded0e0c195cd6a56c6438
elena214 <elebe2104@gmail.com>
2020-12-17 11:47:28 +0100

    Aggiungere i file di progetto.

---
+++ Numeri_Negativi/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Numeri_Negativi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
